91fc36fa4ec8847952153bc001abe2a9b2307c54
Peter Crona <petercrona89@gmail.com>
2021-01-24 17:23:43 +0100

    also read and print analog value

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -5,14 +5,17 @@ WinsenZE25O3 sensor{2};
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("ppb");
+  Serial.println("ppb,analogVal");
 }
 
 void loop() {
   int ppb = sensor.readPPB();
+  int analogVal = analogRead(A0);
 
   if (ppb > 0) {
-    Serial.println(ppb);
+    Serial.print(ppb);
+    Serial.print(",");
+    Serial.println(analogVal);
     delay(1000);
   }
 }
