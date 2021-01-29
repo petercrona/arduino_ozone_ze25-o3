56cfc947e0def3e4f0466e07e6b7db9439af7dea
Peter Crona <petercrona89@gmail.com>
2021-01-29 16:29:48 +0100

    change to use uhoo data

--- analysis/src/main.py
+++ analysis/src/main.py
@@ -7,6 +7,9 @@ from sklearn.linear_model import LinearRegression
 ozoneData = pd.read_csv('../resources/home_ppb_analog_2021-01-25T07:00:00Z',
                         names=['ppb', 'analogVal'])
 
+uhooData = pd.read_csv('../resources/uhoo_20210125-20210126.csv',
+                        sep=';')
+
 berlinData = pd.read_csv('../resources/ber_mc174_20210125-20210126.csv',
                          skiprows=3,
                          sep=';')
@@ -14,25 +17,26 @@ berlinData = pd.read_csv('../resources/ber_mc174_20210125-20210126.csv',
 hourlyData = ozoneData.groupby(ozoneData.index // 3600).mean()
 
 sensorAndBerlinData = pd.concat([hourlyData, berlinData], axis=1)
+uhooAndBerlinData = pd.concat([uhooData, berlinData], axis=1)
 
 model = LinearRegression().fit(
-    sensorAndBerlinData[['analogVal']],
-    sensorAndBerlinData['no2']
+    uhooAndBerlinData[['uhoo_no2']],
+    uhooAndBerlinData['no2']
 )
 
 print('\n= Fitted model:')
 print("f(x) = " + str(model.coef_[0]) + "x + " + str(model.intercept_))
-print('R2 score: ' + str(model.score(sensorAndBerlinData[['analogVal']], sensorAndBerlinData['no2'])))
+print('R2 score: ' + str(model.score(uhooAndBerlinData[['uhoo_no2']], uhooAndBerlinData['no2'])))
 
 print('\n= Correlation between analogVal and NO2:')
 print("Pearson (correlation, pvalue): " +
-      str(scipy.stats.pearsonr(sensorAndBerlinData['analogVal'], sensorAndBerlinData['no2']))
+      str(scipy.stats.pearsonr(uhooAndBerlinData['uhoo_no2'], uhooAndBerlinData['no2']))
 )
-print(scipy.stats.spearmanr(sensorAndBerlinData['analogVal'], sensorAndBerlinData['no2']))
-print(scipy.stats.kendalltau(sensorAndBerlinData['analogVal'], sensorAndBerlinData['no2']))
+print(scipy.stats.spearmanr(uhooAndBerlinData['uhoo_no2'], uhooAndBerlinData['no2']))
+print(scipy.stats.kendalltau(uhooAndBerlinData['uhoo_no2'], uhooAndBerlinData['no2']))
 
 print('')
 
 # Show fancy plot with regression line
-sn.lmplot(x='analogVal', y='no2', data=sensorAndBerlinData, fit_reg=True)
+sn.lmplot(x='uhoo_no2', y='no2', data=uhooAndBerlinData, fit_reg=True)
 plt.show()
