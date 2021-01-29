4944c5f5be61f77c213c11610bd9321a3b28a815
Peter Crona <petercrona89@gmail.com>
2021-01-29 14:53:21 +0100

    remove initial println and add estimated no2

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -5,7 +5,6 @@ WinsenZE25O3 sensor{2};
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("ppb,analogVal");
 }
 
 void loop() {
@@ -15,7 +14,15 @@ void loop() {
   if (ppb > 0) {
     Serial.print(ppb);
     Serial.print(",");
-    Serial.println(analogVal);
+    Serial.print(analogVal);
+    Serial.print(",");
+    Serial.println(estimateIndoorNO2PPB(analogVal));
     delay(1000);
   }
+}
+
+double estimateIndoorNO2PPB(int analogVal) {
+  // based on fitted model for predicting outdoor NO2 and indoor/outdoor ratio from
+  // https://link.springer.com/article/10.1007/s10653-019-00441-0?shared-article-renderer
+  return ((7.51826819982211 * analogVal - 636.9571501802109) * 0.73) / 1.88;
 }
